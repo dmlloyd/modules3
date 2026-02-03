473a94ea4d0af570155ec617d1a781b9cd8dcf6a
David M. Lloyd <dmlloyd@ibm.com>
2026-02-03 08:19:05 -0600

    Minor enhancements

--- src/main/java/io/github/dmlloyd/modules/desc/Modifiers.java
+++ src/main/java/io/github/dmlloyd/modules/desc/Modifiers.java
@@ -36,11 +36,61 @@ public final class Modifiers<M extends Enum<M> & ModifierFlag> {
     }
 
     public boolean contains(M item) {
-        return item != null && (flags & bit(item)) != 0;
+        return containsAny(bit(item));
+    }
+
+    public boolean containsAny(M item0, M item1) {
+        return containsAny(bit(item0) | bit(item1));
+    }
+
+    public boolean containsAny(M item0, M item1, M item2) {
+        return containsAny(bit(item0) | bit(item1) | bit(item2));
+    }
+
+    public boolean containsAny(M item0, M item1, M item2, M item3) {
+        return containsAny(bit(item0) | bit(item1) | bit(item2) | bit(item3));
+    }
+
+    private boolean containsAny(int bits) {
+        return (flags & bits) != 0;
+    }
+
+    @SafeVarargs
+    public final boolean containsAll(M... items) {
+        int bits = 0;
+        for (M item : items) {
+            bits |= bit(item);
+        }
+        return containsAll(bits);
+    }
+
+    public boolean containsAll(M item0, M item1) {
+        return containsAll(bit(item0) | bit(item1));
+    }
+
+    public boolean containsAll(M item0, M item1, M item2) {
+        return containsAll(bit(item0) | bit(item1) | bit(item2));
+    }
+
+    public boolean containsAll(M item0, M item1, M item2, M item3) {
+        return containsAll(bit(item0) | bit(item1) | bit(item2) | bit(item3));
+    }
+
+    @SafeVarargs
+    public final boolean containsAny(M... items) {
+        int bits = 0;
+        for (M item : items) {
+            bits |= bit(item);
+        }
+        return containsAny(bits);
+    }
+
+    private boolean containsAll(int bits) {
+        return (flags & bits) == bits;
     }
 
     private static <M extends Enum<M> & ModifierFlag> int bit(final M item) {
-        return 1 << item.ordinal();
+        return item == null ? 0 : 1 << item.ordinal();
     }
 
     public Modifiers<M> with(M item) {
